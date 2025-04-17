506d9770c174114494b666f49fc14c8f37eb1f08
i5o <i5o@3ds.com>
2025-04-17 17:02:25 +0200

    test variabilization of secret from plugin

--- api/internal/handler/data_loader/route_export.go
+++ api/internal/handler/data_loader/route_export.go
@@ -715,6 +715,22 @@ func (h *Handler) RouteList(c droplet.Context, conf *loader.DataSetsExport) erro
 
 					if ro.Plugins[plugin] != nil {
 						log.Infof("Check Plugins2: %s", ro.Plugins[plugin])
+
+						if pluginMap, ok := ro.Plugins[plugin].(map[string]interface{}); ok {
+							for key, value := range pluginMap {
+								log.Infof("Key: %s, Value: %v\n", key, value)
+								fmt.Printf("Key: %s, Value: %v\n", key, value)
+							
+								// If the value is a nested map, you can assert its type
+								if nestedMap, ok := value.(map[string]interface{}); ok {
+									log.Infof("  Nested Map:")
+									fmt.Println("  Nested Map:")
+									for nestedKey, nestedValue := range nestedMap {
+										log.Infof("    %s: %v\n", nestedKey, nestedValue)
+										fmt.Printf("    %s: %v\n", nestedKey, nestedValue)
+									}
+								}
+						}
 					}
 				}
 			}
